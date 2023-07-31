0597cc5a88e4a64e8d6ba6e757cde49eab2a3811
Rafiul <hredoy.rafiul@gmail.com>
2023-07-31 12:46:58 +0600

    updated problem solving folder

---
+++ src/syllabusProblems/PrefixSumArray.java
@@ -0,0 +1,45 @@
+package syllabusProblems;
+// array[]={2,3,4,5}
+//the expected output is array[]={2,5,7,9}
+
+// the recursive relation is
+
+//arr[n]=arr[n]+arr[n-1]
+public class PrefixSumArray {
+
+   void printArray(int[] arr)
+   {
+
+       for(int j:arr)
+       {
+
+           System.out.println(j);
+
+       }
+   }
+
+int sum=0;
+
+   int[] findSum(int[] arr,int len)
+   {
+       /*this method will return an array
+
+      we need to make such a recursive method which shall
+      everytime calculate the element of the array
+      and place it in the index
+
+       l*/
+
+       if()
+       {
+           sum=arr[len]+findSum(arr,len-1);
+
+       }
+
+
+
+
+   }
+
+
+}
