b13d963b8c32037045ea726ff272619e695e1e99
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-28 17:55:41 +0600

    heap problem added

---
+++ src/syllabusProblems/HeapDemo.java
@@ -0,0 +1,22 @@
+package syllabusProblems;
+
+
+//
+
+public class HeapDemo {
+// 38,31,29,25,24,23,25,
+int[] insertNewNode(int node,int[] tree)
+{
+    //put the node at the index[length]
+    //if the  array[index[length]] is greater than index[]
+    while
+
+
+
+
+
+}
+
+
+
+}
--- src/syllabusProblems/SlidingWind.java
+++ src/syllabusProblems/SlidingWind.java
@@ -3,5 +3,35 @@ package syllabusProblems;
 
 //Sliding Window Maximum | Sliding Window Algorithm | Deque Data Structure | DSA-One Course #54
 
+// the problem  is there will be an array. we will be given a number
+
+
+// the loop will iterate for once and print the sum everytime.that sums will be returned in an array that's why
+// the function's return type is int
+
 public class SlidingWind {
+
+
+
+    int[] windowSum(int range,int[] array)
+    {
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
