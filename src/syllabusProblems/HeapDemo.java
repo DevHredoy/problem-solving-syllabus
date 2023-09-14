a2a5925575f489caa4c28329f13f38e9fe9f61e9
Rafiul <hredoy.rafiul@gmail.com>
2023-09-14 07:02:42 +0600

    updated

--- src/syllabusProblems/HeapDemo.java
+++ src/syllabusProblems/HeapDemo.java
@@ -3,43 +3,47 @@ package syllabusProblems;
 
 //
 
+import java.util.ArrayList;
+
 public class HeapDemo {
-/*
-// 38,31,29,25,24,23,25,
-int[] insertNewNode(int node,int[] tree)
+//// 38,31,29,25,24,23,25,
+int[] insertNewNode(int node, ArrayList<Integer> list)
 {
+
+    list.add(node);
+}
     //put the node at the index[length]
-   // the index of the node will stored in a var,
+  // the index of the node will stored in a var,
+
+
 
     while(True)
-    {
-        if( node is greater than the parent node)
-        {
-
-            // node will be placed in the place of parent node, swapped
-            //update the value of var by the formula var/2
-
-
-        }
-        else
-        {
-            true will turn to false
-
-        }
+   {
+      if( node is greater than the parent node)
+     {
+        // node will be placed in the place of parent node, swapped
+         // update the value of var by the formula var/2
 
 
+   }
+     else
+//        {
+//            true will turn to false
+//
+//        }
+//
+//
+//
+//
+//
+//    }
+//
+//
+//
+//
+//
+//}
+//
 
-
-
-    }
-
-
-
-
-
-}
-
-
-*/
 
 }
